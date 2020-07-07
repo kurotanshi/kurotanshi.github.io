02ce2853a0956766aae3c5cc8345546c9c8b03c8
Kuro Hsu <kurotanshi@gmail.com>
2020-07-08 00:43:49 +0800

    Site updated: 2020-07-08 00:43:48

---
+++ js/fancybox.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('.post img').each(function() {
+    if ($(this).parent().hasClass('fancybox')) return;
+    if ($(this).hasClass('nofancybox')) return;
+    var alt = this.alt;
+    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
+  });
+
+  $(this).find('.fancybox').each(function(){
+    $(this).attr('rel', 'article');
+  });
+
+  $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']").attr('rel', 'gallery').fancybox({
+    helpers : { title: { type: 'inside'} }
+  });
+});
